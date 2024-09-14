9ea6750f77cde36fe170a669ab7e56e198f74acb
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-09-14 16:50:35 +0530

    Feat: Implemented Cart Total

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Title = ({ text1, text2 }) => {
   return (
-    <div className="inline-flex gap-2 items-center mb-3">
+    <div className="mb-3 inline-flex items-center gap-2">
       <p className="text-gray-500">
-        {text1} <span className="text-gray-700 font-medium">{text2}</span>
+        {text1} <span className="font-medium text-gray-700">{text2}</span>
       </p>
-      <p className="w-8 sm:w-12 h-8 sm:h-[2px] bg-gray-700"></p>
+      <p className="h-8 w-8 bg-gray-700 sm:h-[2px] sm:w-12"></p>
     </div>
   );
 };
