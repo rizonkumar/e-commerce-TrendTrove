d28bdd00aa174745eb314d37796f76cf3a76dc5a
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-09-17 09:05:43 +0530

    Feat: Implemented Edit API

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,20 +1,49 @@
-import express from 'express';
-import { addProduct, getAllProducts, removeProduct, getSingleProduct } from '../controllers/productController.js';
-import upload from '../middleware/multer.js';
-import adminAuth from '../middleware/adminAuth.js';
+import express from "express";
+import {
+  addProduct,
+  getAllProducts,
+  removeProduct,
+  getSingleProduct,
+  updateProduct,
+} from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 // Route to add a new product
-productRouter.post('/add', adminAuth, upload.fields([{ name: 'image1', maxCount: 1 }, {name: 'image2', maxCount: 1}, {name: 'image3', maxCount: 1}, {name: 'image4', maxCount: 1}]), addProduct);
+productRouter.post(
+  "/add",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
 
 // Route to get all products
-productRouter.get('/all',adminAuth, getAllProducts);
+productRouter.get("/all", adminAuth, getAllProducts);
 
 // Route to remove a product
-productRouter.delete('/remove',adminAuth, removeProduct);
+productRouter.delete("/remove", adminAuth, removeProduct);
 
 // Route to get a single product
-productRouter.post('/single',adminAuth, getSingleProduct);
+productRouter.post("/single", adminAuth, getSingleProduct);
+
+// Route to update a product
+productRouter.put(
+  "/update",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  updateProduct
+);
 
 export default productRouter;
